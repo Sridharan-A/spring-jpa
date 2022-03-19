9e97365256ced988a7fa75daf459a88d93c83f5f
subash <18euit157@skcet.ac.in>
2022-03-19 10:53:05 +0530

    added from branch -1

--- src/main/java/com/student/StudentApp.java
+++ src/main/java/com/student/StudentApp.java
@@ -3,7 +3,7 @@ package com.student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+//added commet from branch -1
 @SpringBootApplication
 public class StudentApp {
     public static void main(String[] args) {
